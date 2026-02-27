c934bd2e11e101df734d28973ac1c010045ee1dc
campaignreza <campaign.reza@gmail.com>
2026-02-27 20:48:19 +0330

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def health():
 genai.configure(api_key=GEMINI_KEY)
 
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-pro-latest",
+    model_name="gemini-1.0-pro",
     system_instruction=MASTER_INSTRUCTIONS
 )
 
